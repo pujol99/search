c16e6ec3eb39750b98b0114e61b6cbef1430d6bc
paubaiges <pau.baiges01@estudiant.upf.edu>
2020-10-16 16:07:55 +0200

    prueba

--- autograder.py
+++ autograder.py
@@ -8,7 +8,7 @@
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# Student side autograding was added by Brad Miller, Nick Hay, and Peter
+# Student side autograding was added by Brad Miller, Nick Hay, and 
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
 
